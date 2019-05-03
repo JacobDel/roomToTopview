22c8fabded937310fad8710e6b2284e7f239ff5f
Jacob <jacob.delabie@hotmail.com>
2019-05-03 09:02:27 +0200

    sjalam

--- Parse/ParseToLAS.py
+++ Parse/ParseToLAS.py
@@ -7,29 +7,34 @@ import math
 import shutil
 
 def writeToLAS(depth):
-    folderNumber = 1
     header = laspy.header.Header(point_format=2)
-    pt = laspy.file.File("room.las", mode="w", header=header)
-    degreePerPixelY = (float)(360 / (float)(len(depth[0])))  # pixel location to degree calculation
+    pt = laspy.file.File("output.las", mode="w", header=header)
+    degreePerPixelY = (float)(45 / (float)(len(depth[0])))  # pixel location to degree calculation
     degreePerPixelX = (float)(360 / (float)(len(depth)))
 
     # convert all the points
-    ptX=[]
-    ptY=[]
-    ptZ=[]
+    ptX= np.array([])
+    ptY=np.array([])
+    ptZ=np.array([])
+    ptInt = np.array([])
     for x in range(0, len(depth[0])):
         for y in range(0, len(depth)):
             alphaX = x * degreePerPixelX * math.pi / 180  # calculate the "angle" in the panorama, python math works in radians..
-            alphaY = x * degreePerPixelY * math.pi / 180  # calculate the "angle" in the panorama, python math works in radians..
-            ptX = np.append(ptX,depth[x][y] * math.cos(alphaY) * math.sin(alphaX))
-            ptY = np.append(ptY, math.sin(alphaY) * depth[x][y])
-            ptZ = np.append(ptZ,depth[x][y] * math.cos(alphaY) * math.cos(alphaX))
-            pt.x = ptX
-            pt.y = ptY
-            pt.z = ptZ
+            alphaY = y * degreePerPixelY * math.pi / 180  # calculate the "angle" in the panorama, python math works in radians..
+            ptX = np.append(ptX,depth[y][x] * math.cos(alphaY) * math.sin(alphaX))
+            ptY = np.append(ptY, math.sin(alphaY) * depth[y][x])
+            ptZ = np.append(ptZ,depth[y][x] * math.cos(alphaY) * math.cos(alphaX))
+            ptInt = np.append(ptInt,depth[y][x])
+    pt.header.offset = [0,0,0]
+    pt.header.scale = [0.001, 0.001, 0.001]
+    pt.x = ptZ
+    pt.y = ptY
+    pt.z = ptX
+    pt.intensity = ptInt
+    pt.Intensity = ptInt
     pt.close()
 
-    potreePath = os.getcwd() + "\potreeFolder"+str(folderNumber)
+    potreePath = os.getcwd() + "\potreeFolder"
     if os.path.exists(potreePath):
         shutil.rmtree(potreePath)  # delete the old data
     os.mkdir(potreePath)
