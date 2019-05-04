230082f5c6c0275afc493fa01f0039834c5ff6eb
Jacob <jacob.delabie@hotmail.com>
2019-05-04 10:20:31 +0200

    bim parsing.. beetje alles wat we tot nu toe kunnen doen i.v.m. bim parsen

--- Parse/ParseBim.py
+++ Parse/ParseBim.py
@@ -1,6 +1,41 @@
 from rpw import db
+
 def getObjects():
-    levels = db.Collector(of_category='Levels', is_type=True)
+    # levels = db.Collector(of_category='Levels', is_type=True)
     walls = db.Collector(of_class='Wall', where=lambda x: x.parameters['Length'] > 5)
     desks = db.Collector(of_class='FamilyInstance', level='Level 1')
-    return levels,walls,desks
+    windows = db.Collector(of_class='Window')
+    return windows,walls,desks
+
+def equalsBIM(rWalls,rWindows,rDesks):
+    #copy the objects
+    qWalls = rWalls
+    qWindows=rWindows
+    qDesks=rDesks
+
+    #get BIM objects
+    bWindows,bWalls,bDesks=getObjects()
+
+    #go through every object in our BIM & search an equal object
+    print("Compare walls..")
+    compareObjectLists(qWalls,bWalls)
+    print("Compare desks..")
+    compareObjectLists(qDesks,bDesks)
+    print("Compare windows..")
+    compareObjectLists(qWindows,bWindows)
+
+def compareObjectLists(copyList,bimList): #compare a category of objects from the bim model and a copylist (a copy of the objects in that category we found)
+    for bimobj in bimList:
+        # improve this algorithm to also accept objects with slightly variation on size
+        found=False
+        for i in range(0,len(copyList)):
+            if(copyList[i]==bimobj):
+                found=True
+                break
+        if(found==False):
+            print("Could not find the following object from our BIM model: ")
+            print(bimobj)
+    if(len(copyList)>0):
+        print("There are new objects in the room: ")
+        for obj in copyList:
+            print obj
